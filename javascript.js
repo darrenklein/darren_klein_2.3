90837277c76c1808a1f81439b9fa0fde96da152e
Darren Klein <darren.a.klein@gmail.com>
2016-09-20 11:14:15 -0400

    Commit success! Initial commit

---
+++ javascript.js
@@ -0,0 +1,58 @@
+var clock = document.getElementById("clock");
+var millisecondContainer = document.getElementById("millisecond");
+var timeStyle = "twentyfour";
+var date = new Date();
+clock.innerHTML = tellTime(date.getHours(), date.getMinutes(), date.getSeconds());
+millisecondContainer.innerHTML = getMilliseconds(date.getMilliseconds());
+
+function tellTime(hours, minutes, seconds){
+	if(timeStyle === "twelve" && hours > 12){
+		hours = hours - 12;
+	};
+	
+	for(i = 0; i < arguments.length; i++){
+		if(arguments[i].toString().length === 1){
+			arguments[i] = "0" + arguments[i];
+		};
+	};
+
+	time = hours + ":" + minutes + ":" + seconds;
+	return time;
+};
+
+function getMilliseconds(milliseconds){
+	if(milliseconds.toString().length === 1){
+		milliseconds = "00" + milliseconds;
+	}
+	else if(milliseconds.toString().length === 2){
+		milliseconds = "0" + milliseconds;
+	}
+	else{
+		milliseconds = milliseconds;
+	};
+	return milliseconds;
+};
+
+window.setInterval(function(){
+	var date = new Date();
+	clock.innerHTML = tellTime(date.getHours(), date.getMinutes(), date.getSeconds());
+}, 1000);
+
+window.setInterval(function(){
+	var date = new Date();
+	millisecondContainer.innerHTML = getMilliseconds(date.getMilliseconds());
+}, 1);
+
+function hoursChange(element){
+	if(element.getAttribute("hours") === "twentyfour"){
+		timeStyle = "twelve";
+		element.setAttribute("hours", timeStyle);
+		element.innerHTML = "twelve hour";
+	}
+	else{
+		timeStyle = "twentyfour";
+		element.setAttribute("hours", timeStyle);element.innerHTML = "twenty-four hour";
+	};
+};
+
+document.getElementById("hours_change").addEventListener("click", function(){hoursChange(this)});
