e780c542656582e7200920f2505cb13788ccf37a
Darren Klein <darren.a.klein@gmail.com>
2016-10-06 23:01:52 -0400

    Commit success! Got close, but trying it as an object.

--- javascript.js
+++ javascript.js
@@ -142,113 +142,155 @@
 
 
 
-var loadClock = function(time){
-	var clockContainer = document.getElementById("clock");
-	var millisecondContainer = document.getElementById("millisecond");
-	var hours_change_button = document.getElementById("hours_change");
-	var am = document.getElementById("am");
-	var pm = document.getElementById("pm");
-	var timeStyle = "twentyfour";
-	var hours = time[0];
-	var minutes = time[1];
-	var seconds = time[2];
-	var milliseconds = time[3];
-	var lightGreen = "#00FF74";
-	var darkGray = "#454545";
-	var period;
+// var loadClock = function(time){
+// 	var clockContainer = document.getElementById("clock");
+// 	var millisecondContainer = document.getElementById("millisecond");
+// 	var hours_change_button = document.getElementById("hours_change");
+// 	var am = document.getElementById("am");
+// 	var pm = document.getElementById("pm");
+// 	var timeStyle = "twentyfour";
+// 	var hours = time[0];
+// 	var minutes = time[1];
+// 	var seconds = time[2];
+// 	var milliseconds = time[3];
+// 	var lightGreen = "#00FF74";
+// 	var darkGray = "#454545";
+// 	var period;
 
 
-	if(hours < 12){
-		var period = "am";
-	}
-	else{
-		var period = "pm";
-	};
+// 	if(hours < 12){
+// 		var period = "am";
+// 	}
+// 	else{
+// 		var period = "pm";
+// 	};
 
-	//SET DISPLAY BASED ON CLOCK MODE
-	// if(timeStyle === "twelve" && hours === 0){
-	// 	hours = 12;
-	// }
-	// else if(timeStyle === "twelve" && hours > 12){
-	// 	hours = hours - 12;
-	// }
-	// else{
-	// 	hours = hours;
-	// };
+// 	//SET DISPLAY BASED ON CLOCK MODE
+// 	// if(timeStyle === "twelve" && hours === 0){
+// 	// 	hours = 12;
+// 	// }
+// 	// else if(timeStyle === "twelve" && hours > 12){
+// 	// 	hours = hours - 12;
+// 	// }
+// 	// else{
+// 	// 	hours = hours;
+// 	// };
 
 
-	function hoursChange(element){
-		if(element.getAttribute("hours") === "twentyfour"){
-			timeStyle = "twelve";
-			element.setAttribute("hours", timeStyle);
-			element.innerHTML = "twelve hour";
+// 	function hoursChange(element){
+// 		if(element.getAttribute("hours") === "twentyfour"){
+// 			timeStyle = "twelve";
+// 			element.setAttribute("hours", timeStyle);
+// 			element.innerHTML = "twelve hour";
 
-			//SET DISPLAY AND STYLE BASED ON CLOCK MODE/ACTUAL TIME
-			if(period === "pm"){
-				pm.style.color = lightGreen;
-				am.style.color = darkGray;
-			}
-			else{
-				am.style.color = lightGreen;
-				pm.style.color = darkGray;
-			};
+// 			//SET DISPLAY AND STYLE BASED ON CLOCK MODE/ACTUAL TIME
+// 			if(period === "pm"){
+// 				pm.style.color = lightGreen;
+// 				am.style.color = darkGray;
+// 			}
+// 			else{
+// 				am.style.color = lightGreen;
+// 				pm.style.color = darkGray;
+// 			};
+// 		}
+// 		else{
+// 			timeStyle = "twentyfour";
+// 			element.setAttribute("hours", timeStyle);element.innerHTML = "twenty-four hour";
+// 			am.style.color = darkGray;
+// 			pm.style.color = darkGray;
+// 		};
+// 	};
+
+
+// 	clockContainer.innerHTML = hours + ":" + minutes + ":" + seconds;
+// 	millisecondContainer.innerHTML = milliseconds;
+
+// 	hours_change_button.onclick = function(){
+// 		hoursChange(this);
+// 	};
+
+// 	window.setInterval(function(){
+// 		loadClock(getTime());
+// 	}, 1);
+// };
+
+
+
+
+
+
+// var getTime = function(){
+// 	var date = new Date();
+// 	var hours = date.getHours();
+// 	var minutes = date.getMinutes();
+// 	var seconds = date.getSeconds();
+// 	var milliseconds = date.getMilliseconds();
+
+// 	function buildTime(hours, minutes, seconds, milliseconds){
+// 		for(i = 0; i < arguments.length; i++){
+// 			//LEADING ZEROS FOR MILLISECONDS
+// 			if(i === arguments.length - 1){
+// 				if(arguments[i].toString().length === 1){
+// 					arguments[i] = "00" + arguments[i];
+// 				}
+// 				else if(arguments[i].toString().length === 2){
+// 					arguments[i] = "0" + arguments[i];
+// 				};
+// 			}
+// 			//LEADING ZEROS FOR HOURS, MINUTES, SECONDS
+// 			else{
+// 				if(arguments[i].toString().length === 1){
+// 					arguments[i] = "0" + arguments[i];
+// 				};
+// 			};
+// 		};
+		
+// 		return [hours, minutes, seconds, milliseconds];
+// 	};
+
+// 	return buildTime(hours, minutes, seconds, milliseconds);
+// };
+
+// loadClock(getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//AS AN OBJECT...
+var clock = {
+	"getTime": function(){
+		var date = new Date();
+		var hour = date.getHours();
+		var minute = date.getMinutes();
+		var second = date.getSeconds();
+		var millisecond = date.getMilliseconds();
+
+		if(hour < 12){
+			this.period = "am";
 		}
 		else{
-			timeStyle = "twentyfour";
-			element.setAttribute("hours", timeStyle);element.innerHTML = "twenty-four hour";
-			am.style.color = darkGray;
-			pm.style.color = darkGray;
+			this.period = "pm";
 		};
-	};
 
-
-	clockContainer.innerHTML = hours + ":" + minutes + ":" + seconds;
-	millisecondContainer.innerHTML = milliseconds;
-
-	hours_change_button.onclick = function(){
-		hoursChange(this);
-	};
-
-	window.setInterval(function(){
-		loadClock(getTime());
-	}, 1);
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+		this.millisecond = millisecond;
+	},
+	"timeStyle_button": {
+		"timeStyle": "twentyfour"
+	}
 };
 
-
-
-
-
-
-var getTime = function(){
-	var date = new Date();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var seconds = date.getSeconds();
-	var milliseconds = date.getMilliseconds();
-
-	function buildTime(hours, minutes, seconds, milliseconds){
-		for(i = 0; i < arguments.length; i++){
-			//LEADING ZEROS FOR MILLISECONDS
-			if(i === arguments.length - 1){
-				if(arguments[i].toString().length === 1){
-					arguments[i] = "00" + arguments[i];
-				}
-				else if(arguments[i].toString().length === 2){
-					arguments[i] = "0" + arguments[i];
-				};
-			}
-			//LEADING ZEROS FOR HOURS, MINUTES, SECONDS
-			else{
-				if(arguments[i].toString().length === 1){
-					arguments[i] = "0" + arguments[i];
-				};
-			};
-		};
-		
-		return [hours, minutes, seconds, milliseconds];
-	};
-
-	return buildTime(hours, minutes, seconds, milliseconds);
-};
-
-loadClock(getTime());
+clock.getTime();
+console.log(clock);
